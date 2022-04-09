350f78bfe8162d1d40428bb64f9248a9b0953330
fawazorg <foazorg@gmail.com>
2022-04-09 05:05:30 +0300

    Export Message

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const Client = require("./Client");
 const Group = require("./Models/Group/Group");
 const Subscriber = require("./Models/Subscriber/Subscriber");
+const Message = require("./Models/Message/Message");
 const Constants = require("./Constants");
 
 module.exports = {
   Client,
   Group,
+  Message,
   Subscriber,
   Constants,
 };
