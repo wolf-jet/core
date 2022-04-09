c1c1ce48f3acf9af1cc3bcaf88657b30b0008325
fawazorg <foazorg@gmail.com>
2022-04-09 04:37:42 +0300

    Export Group & Subscriber

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const Client = require("./Client");
+const Group = require("./Models/Group/Group");
+const Subscriber = require("./Models/Subscriber/Subscriber");
 const Constants = require("./Constants");
 
 module.exports = {
   Client,
+  Group,
+  Subscriber,
   Constants,
 };
